140adb92595c0f620e872ccf8825bf71dc29736a
Mulliw <rune_willum@msn.com>
2025-01-07 11:54:28 +0100

    op

--- afprøvning.ps1
+++ afprøvning.ps1
@@ -1,37 +1,32 @@
-# Sti til CSV-filen med brugerliste
+# Placering af csv filen med brugere
 $csvFile = "C:\Users\Rune_\OneDrive\Dokumenter\Server Auto2\Users.csv"
 
-# Sti til logfil
+# Placering af logfilen
 $logFile = "C:\Users\Rune_\OneDrive\Dokumenter\Server Auto2\Logsoutput\logfile.txt"
 $logDirectory = [System.IO.Path]::GetDirectoryName($logFile)
 
-# Opret log-mappen, hvis den ikke allerede eksisterer
+# Her er der lavet en if, til at oprette en logfil hvis ikke der er en i forvejen.
 if (-not (Test-Path $logDirectory)) {
     New-Item -ItemType Directory -Path $logDirectory
 }
 
-# Start logging
-Start-Transcript -Path $logFile
-
-# Indlæs brugere fra CSV-fil
+# Her starter vi med at oprette bruger ud fra vores csv-fil
 $users = Import-Csv -Path $csvFile
 
 foreach ($user in $users) {
     try {
-        # Her oprettes brugeren (dette er bare et eksempel på kommando)
-        # Udskift denne del med din brugeroprettelseslogik
-        New-ADUser -SamAccountName $user.Username -GivenName $user.FirstName -Surname $user.LastName -EmailAddress $user.Email -AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -Force) -Enabled $true
+        # Her oprettes brugeren ud fra de parameter vi har i vores csv-fil    
+        New-ADUser -SamAccountName $user.Username -GivenName $user.FirstName -Surname $user.LastName -EmailAddress $user.Email
 
-        # Hvis brugeroprettelsen er succesfuld, log det
+        # Hvis brugeroprettelsen er succesfuld, skrives det i loggen
         $message = "Bruger oprettet: $($user.FirstName) $($user.LastName) ($($user.Username)) med email $($user.Email)"
         $message | Out-File -FilePath $logFile -Append
     }
     catch {
-        # Hvis der opstår en fejl, log den
+        # Hvis der opstår en fejl, skrives det i loggen
         $errorMessage = "Fejl ved oprettelse af bruger: $($user.FirstName) $($user.LastName) ($($user.Username)). Fejl: $($_.Exception.Message)"
         $errorMessage | Out-File -FilePath $logFile -Append
     }
 }
 
-# Stop logging
-Stop-Transcript
+
